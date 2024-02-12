b76e60365b63249cdffc766c9f4807f7b9580442
cosmos hagan <cosmoshagan@gmail.com>
2024-02-12 23:29:40 +0000

    Update Model Customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -9,6 +9,16 @@ class Customer extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'type',
+        'email',
+        'phone',
+        'address',
+        'city',
+        'region',
+    ];
+
     public function invoices()
     {
         return $this->hasMany(Invoice::class);
